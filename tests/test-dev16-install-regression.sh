6d9d929d213f19711c3e605ead2e66a6c2201ec8
Lycidias93 <42116872+Lycidias93@users.noreply.github.com>
2026-08-09 16:58:00 +0200

    ci: decouple Dev.16 install test from release version

--- tests/test-dev16-install-regression.sh
+++ tests/test-dev16-install-regression.sh
@@ -73,8 +73,12 @@ if grep -Fq '_src="$1"; _dst="$2"' "$collector"; then
 fi
 pass collector_copy_helpers_do_not_overwrite_outer_destination
 
-grep -Fq 'version=2.0.0' "$module_prop"
-grep -Fq 'versionCode=1016240' "$module_prop"
+module_version="$(sed -n 's/^version=//p' "$module_prop" | head -n 1)"
+module_version_code="$(sed -n 's/^versionCode=//p' "$module_prop" | head -n 1)"
+[[ "$(grep -c '^version=' "$module_prop")" -eq 1 ]] || fail module_version_count
+[[ "$(grep -c '^versionCode=' "$module_prop")" -eq 1 ]] || fail module_version_code_count
+[[ "$module_version" =~ ^[0-9]+[.][0-9]+[.][0-9]+([.-][0-9A-Za-z.-]+)?$ ]] || fail module_version_format
+[[ "$module_version_code" =~ ^[0-9]+$ ]] || fail module_version_code_format
 pass stable_metadata_preserves_dev16_regression
 
 printf '%s\n' 'RESULT: PIXEL_THERMAL_DEV16_INSTALL_REGRESSION_PASS'
